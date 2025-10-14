7c2d8cfe9cd13dcbd944d0680f5625ce454f3736
PauloXAngel <pauloangeloads@gmail.com>
2025-10-14 00:00:45 -0300

    Avanços significativos nos meus estudos de POO

---
+++ prog_ori_obj/ExemploExplorar/Models/Curso.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorar.Models
+{
+    public class Cursos
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+
+        public void AdicionarAluno(Pessoa aluno)
+        {
+            Alunos.Add(aluno);
+        }
+    }
+}
--- prog_ori_obj/ExemploExplorar/Program.cs
+++ prog_ori_obj/ExemploExplorar/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploExplorar.Models;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Paulo";
+p1.Sobrenome = "Angelo";
+p1.Idade = 26;
+p1.Apresentar();
+
