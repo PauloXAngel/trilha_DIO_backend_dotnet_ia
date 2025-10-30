47399080817dafeca68f47d77cd6ff8f276bfc46
PauloXAngel <pauloangeloads@gmail.com>
2025-10-29 23:59:56 -0300

    Introdução aos estudos de POO

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿using ExemploPOO.Models;
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(5000);
+c1.ExibirSaldo();
+
+ 
+
+// // um objeto é uma concretização  de uma representação 
+// Pessoa p1 = new Pessoa(); //Criando objeto de uma classe / P1 se torna objeto da Pessoa
+// p1.Nome = "Paulo";
+// p1.Idade = 26;
+
+// p1.Apresentar();
